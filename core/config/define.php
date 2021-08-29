7b66d5c14b310c195e8e3d5b228753b528e5e310
fedeghe <fedeghe@gmail.com>
2021-08-29 11:24:22 +0200

    make it explicit ERROR_VISIBLE

--- core/config/define.php
+++ core/config/define.php
@@ -12,7 +12,7 @@ defined('ON_DOMAIN') || define('ON_DOMAIN', true);
 defined('ONLINE') || define('ONLINE', true);
 
 defined('DB') || define('DB', 'local');
-	
+
 
 
 defined('DS') || define('DS', DIRECTORY_SEPARATOR);
@@ -22,41 +22,40 @@ defined('DEFAULT_ACTION') || define('DEFAULT_ACTION', 'index');
 defined('URL_EXT') || define('URL_EXT', "/\.html|\.php/");
 defined('CONTROLLER404') || define('CONTROLLER404', 'ctrl404');
 
-defined('PATH_ROOT') || define('PATH_ROOT', realpath(dirname(__FILE__).DS.'..'.DS.'..'.DS).DS);
-defined('PATH_VIEW') || define('PATH_VIEW', PATH_ROOT.'app'.DS.'views'.DS);
-defined('PATH_MODEL') || define('PATH_MODEL', PATH_ROOT.'app'.DS.'models'.DS);
-defined('PATH_CONTROLLER') || define('PATH_CONTROLLER', PATH_ROOT.'app'.DS.'controllers'.DS);
-defined('PATH_CONFIG') || define('PATH_CONFIG', PATH_ROOT.'core'.DS.'config'.DS);
-defined('PATH_APP') || define('PATH_APP', PATH_ROOT.'app'.DS);
-defined('PATH_CORE') || define('PATH_CORE', PATH_ROOT.'core'.DS);
-defined('PATH_TRANSLATIONS') || define('PATH_TRANSLATIONS', PATH_ROOT.'app'.DS.'i18n'.DS);
-defined('PATH_CHU') || define('PATH_CHU', PATH_APP.'chunks'.DS);
-defined('PATH_CHU_SIS') || define('PATH_CHU_SIS', PATH_CORE.'lib'.DS.'chunks'.DS);
-defined('PATH_SNI') || define('PATH_SNI', PATH_APP.'snippets'.DS);
-defined('PATH_SNI_SIS') || define('PATH_SNI_SIS', PATH_CORE.'lib'.DS.'snippets'.DS);
+defined('PATH_ROOT') || define('PATH_ROOT', realpath(dirname(__FILE__) . DS . '..' . DS . '..' . DS) . DS);
+defined('PATH_VIEW') || define('PATH_VIEW', PATH_ROOT . 'app' . DS . 'views' . DS);
+defined('PATH_MODEL') || define('PATH_MODEL', PATH_ROOT . 'app' . DS . 'models' . DS);
+defined('PATH_CONTROLLER') || define('PATH_CONTROLLER', PATH_ROOT . 'app' . DS . 'controllers' . DS);
+defined('PATH_CONFIG') || define('PATH_CONFIG', PATH_ROOT . 'core' . DS . 'config' . DS);
+defined('PATH_APP') || define('PATH_APP', PATH_ROOT . 'app' . DS);
+defined('PATH_CORE') || define('PATH_CORE', PATH_ROOT . 'core' . DS);
+defined('PATH_TRANSLATIONS') || define('PATH_TRANSLATIONS', PATH_ROOT . 'app' . DS . 'i18n' . DS);
+defined('PATH_CHU') || define('PATH_CHU', PATH_APP . 'chunks' . DS);
+defined('PATH_CHU_SIS') || define('PATH_CHU_SIS', PATH_CORE . 'lib' . DS . 'chunks' . DS);
+defined('PATH_SNI') || define('PATH_SNI', PATH_APP . 'snippets' . DS);
+defined('PATH_SNI_SIS') || define('PATH_SNI_SIS', PATH_CORE . 'lib' . DS . 'snippets' . DS);
 
-defined('PATH_CACHE') || define('PATH_CACHE', PATH_ROOT.'cache'.DS);
+defined('PATH_CACHE') || define('PATH_CACHE', PATH_ROOT . 'cache' . DS);
 defined('CACHE_ACTIVE') || define('CACHE_ACTIVE', false);
 
 
 defined('AUTO_PARSE') || define('AUTO_PARSE', true);
 defined('PROTOCOL') || define('PROTOCOL', 'http' . (((array_key_exists('HTTPS', $_SERVER) && $_SERVER['HTTPS'] == 'on')) ? 's' : ''));
 defined('LOCAL_FOLDER') || define('LOCAL_FOLDER', ON_DOMAIN ? '' : basename(dirname(dirname(dirname(__FILE__)))));
-defined('BASE_FOLDER') || define('BASE_FOLDER', ON_DOMAIN ? '' :  LOCAL_FOLDER . US );
+defined('BASE_FOLDER') || define('BASE_FOLDER', ON_DOMAIN ? '' :  LOCAL_FOLDER . US);
 defined('URL_ROOT') || define('URL_ROOT', PROTOCOL . ':' . US . US . $SERVER_NAME . US);
 defined('URL_BASE') || define('URL_BASE', URL_ROOT . BASE_FOLDER);
 defined('URL_COMPLETE') || define('URL_COMPLETE', $REQUEST_URI);
 defined('URL') || define('URL', parse_url($REQUEST_URI)['path']);
 
-defined('PATH_JS') || define('PATH_JS', PATH_ROOT.'htdocs'.DS.'js'.DS);
-defined('PATH_CSS') || define('PATH_CSS', PATH_ROOT.'htdocs'.DS.'css'.DS);
+defined('PATH_JS') || define('PATH_JS', PATH_ROOT . 'htdocs' . DS . 'js' . DS);
+defined('PATH_CSS') || define('PATH_CSS', PATH_ROOT . 'htdocs' . DS . 'css' . DS);
 
 
 defined('DEFAULT_LANG') || define('DEFAULT_LANG', 'en');
 //  activate to pick up labels and get files for lang
 defined('CUMULATE_LANG') || define('CUMULATE_LANG', true);
 
-defined('ERROR_LEVEL') || define('ERROR_LEVEL', 1/* 2<<13 -1 */ );
+defined('ERROR_LEVEL') || define('ERROR_LEVEL', 1/* 2<<13 -1 */);
+defined('ERROR_VISIBLE') || define('ERROR_VISIBLE', true);
 defined('STACKTRACE_ENABLED') || define('STACKTRACE_ENABLED', true);
-
-
--- core/config/error_handling.php
+++ core/config/error_handling.php
@@ -3,64 +3,64 @@
 error_reporting(0);
 
 // user defined error handling function
-function userErrorHandler($errno, $errmsg, $filename, $linenum, $vars){
+function userErrorHandler($errno, $errmsg, $filename, $linenum, $vars)
+{
 
     ob_start();
     $err_level =  2 << ERROR_LEVEL - 1;
-    
-    if($errno <= $err_level){
-        
-        $err =  'Into '.__FUNCTION__.'() at line '.__LINE__.
-          "\n\n---ERRNO---\n". print_r( $errno, true).
-          "\n\n---ERRSTR---\n". print_r( $errmsg, true).
-          "\n\n---ERRFILE---\n". print_r( $filename, true).
-          "\n\n---ERRLINE---\n". print_r( $linenum, true).
-          "\n\n---ERRCONTEXT---\n".print_r( $vars, true);
-        
+
+    if (ERROR_VISIBLE && $errno <= $err_level) {
+
+        $err =  'Into ' . __FUNCTION__ . '() at line ' . __LINE__ .
+            "\n\n---ERRNO---\n" . print_r($errno, true) .
+            "\n\n---ERRSTR---\n" . print_r($errmsg, true) .
+            "\n\n---ERRFILE---\n" . print_r($filename, true) .
+            "\n\n---ERRLINE---\n" . print_r($linenum, true) .
+            "\n\n---ERRCONTEXT---\n" . print_r($vars, true);
+
         $styles = array(
-            'common'=>array(
-                'padding-left'=>    '5px',
-                'margin-left'=>     '5px',
-                'margin-bottom'=>   '1px',
-                'color'=>           'black',
-                'background-color'=>'white',
-                'position'=>        'relative',
-                'overflow'=>'scroll'
+            'common' => array(
+                'padding-left' =>    '5px',
+                'margin-left' =>     '5px',
+                'margin-bottom' =>   '1px',
+                'color' =>           'black',
+                'background-color' => 'white',
+                'position' =>        'relative',
+                'overflow' => 'scroll'
             ),
             'green' => array(
-                'border-top'=>      '5px solid green',
-                'border-left'=>     '5px solid green',
-                'border-bottom'=>   '1px dotted green'
+                'border-top' =>      '5px solid green',
+                'border-left' =>     '5px solid green',
+                'border-bottom' =>   '1px dotted green'
             ),
             'red' => array(
-                'border-bottom'=>       '5px solid red',
-                'border-left'=>     '5px solid red',
-                'border-top'=>  '1px dotted red',
-                'margin-bottom'=>   '50px'
+                'border-bottom' =>       '5px solid red',
+                'border-left' =>     '5px solid red',
+                'border-top' =>  '1px dotted red',
+                'margin-bottom' =>   '50px'
             ),
         );
         $green_style = '';
-        foreach(array_merge($styles['common'], $styles['green']) as $sel =>$val)$green_style.=$sel.':'.$val.';';
+        foreach (array_merge($styles['common'], $styles['green']) as $sel => $val) $green_style .= $sel . ':' . $val . ';';
 
         $red_style = '';
-        foreach(array_merge($styles['common'], $styles['red']) as $sel =>$val)$red_style.=$sel.':'.$val.';';
-        
-        echo '<div style="'.$green_style.'"><pre>'.$err.'</pre></div>';
-        
-        if(STACKTRACE_ENABLED){
-            echo '<div style="'.$red_style.'">STACKTRACE:<pre>'.
-                "\n\nBacktrace of errorHandler()\n".debug_print_backtrace()
-                .'</pre></div>';
-        }        
+        foreach (array_merge($styles['common'], $styles['red']) as $sel => $val) $red_style .= $sel . ':' . $val . ';';
+
+        echo '<div style="' . $green_style . '"><pre>' . $err . '</pre></div>';
+
+        if (STACKTRACE_ENABLED) {
+            echo '<div style="' . $red_style . '">STACKTRACE:<pre>' .
+                "\n\nBacktrace of errorHandler()\n" . debug_print_backtrace()
+                . '</pre></div>';
+        }
     }
-    
+
     DSMVC::$errors = ob_get_clean();
 
     //if(DSMVC::$errors) error_log(DSMVC::$errors, 3, "../php_errors.log");
-    if (DSMVC::$errors) { 
+    if (DSMVC::$errors) {
         echo DSMVC::$errors;
     }
-
 }
 
 $old_error_handler = set_error_handler("userErrorHandler");
