932d82f02b488d98f5dcc57eed4b40d8126f96e6
fedeghe <fedeghe@gmail.com>
2024-07-16 23:07:16 +0200

    Revert "x"
    This reverts commit f5d6ecf79250c290387b36ed080e8925cfd196d8.

--- app/controllers/ctrl404.php
+++ app/controllers/ctrl404.php
@@ -3,8 +3,7 @@
 class ctrl404 extends Controller{
 
     public function action_index($_, $url){
-        // Response::send('PAGE NOT FOUND:'. __FILE__);
-        Response::send('PAGE NOT FOUND');
+        Response::send('PAGE NOT FOUND:'. __FILE__);
         Response::send('<br/>');
         Response::send('url :'. $url);
     }
--- app/controllers/index.php
+++ app/controllers/index.php
@@ -1,11 +1,57 @@
 <?php
+
 class index extends Controller{
-    public function action_index($params){   
+
+    public function action_index($params){
+        
         $x = DSMVC::getView('head');
-        $x->title = "gzpot";
+        $Person = new Person('Federico', 'Ghedina');
+        $x->title = "DSmvc ~ " . $Person->sayHello();
+
         $v = DSMVC::getView('default');
         $v->head = $x->display();
-        $v->d = "heloooowwww";
+
+        $_SESSION['user'] = array(
+            'fk_group' => 2
+        );
+
+        // only if the trial db is set
+        // $places = Places::getAll();
+        // $v->data = utility::print_d($places, true);
+
         Response::send($v->display());
     }
+
+
+    public function action_prova(){
+        $x = new View('head');
+        $x->title="prova";
+
+        $v = new View('default');
+
+        $v->head = $x->display();
+
+        if($this->nome!='')$v->nome = $this->nome;
+
+        Response::send($v->display());
+    }
+    
+    public function after_index(){
+        // Response::send(__FILE__.' - method<br />');
+    }
+    public function before_index(){
+        // Response::send(__FILE__.' - method<br />');
+    }
+
+    public function after(){
+        parent::after();
+        // Response::send(__FILE__.' - controller<br />');
+    }
+
+    public function before(){
+        parent::after();
+        // Response::send(__FILE__.' - controller<br />');
+    }
+    
+
 }
