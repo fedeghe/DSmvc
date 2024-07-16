f5d6ecf79250c290387b36ed080e8925cfd196d8
fedeghe <fedeghe@gmail.com>
2024-07-16 22:59:23 +0200

    x

--- app/controllers/ctrl404.php
+++ app/controllers/ctrl404.php
@@ -3,7 +3,8 @@
 class ctrl404 extends Controller{
 
     public function action_index($_, $url){
-        Response::send('PAGE NOT FOUND:'. __FILE__);
+        // Response::send('PAGE NOT FOUND:'. __FILE__);
+        Response::send('PAGE NOT FOUND');
         Response::send('<br/>');
         Response::send('url :'. $url);
     }
--- app/controllers/index.php
+++ app/controllers/index.php
@@ -1,57 +1,11 @@
 <?php
-
 class index extends Controller{
-
-    public function action_index($params){
-        
+    public function action_index($params){   
         $x = DSMVC::getView('head');
-        $Person = new Person('Federico', 'Ghedina');
-        $x->title = "DSmvc ~ " . $Person->sayHello();
-
+        $x->title = "gzpot";
         $v = DSMVC::getView('default');
         $v->head = $x->display();
-
-        $_SESSION['user'] = array(
-            'fk_group' => 2
-        );
-
-        // only if the trial db is set
-        // $places = Places::getAll();
-        // $v->data = utility::print_d($places, true);
-
+        $v->d = "heloooowwww";
         Response::send($v->display());
     }
-
-
-    public function action_prova(){
-        $x = new View('head');
-        $x->title="prova";
-
-        $v = new View('default');
-
-        $v->head = $x->display();
-
-        if($this->nome!='')$v->nome = $this->nome;
-
-        Response::send($v->display());
-    }
-    
-    public function after_index(){
-        // Response::send(__FILE__.' - method<br />');
-    }
-    public function before_index(){
-        // Response::send(__FILE__.' - method<br />');
-    }
-
-    public function after(){
-        parent::after();
-        // Response::send(__FILE__.' - controller<br />');
-    }
-
-    public function before(){
-        parent::after();
-        // Response::send(__FILE__.' - controller<br />');
-    }
-    
-
 }
