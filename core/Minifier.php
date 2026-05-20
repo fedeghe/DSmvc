a8fe7f9fe7d471d0988e1beb8ce17f377cd20a75
fedeghe <fedeghe@gmail.com>
2026-05-20 22:56:52 +0200

    fix

--- core/Minifier.php
+++ core/Minifier.php
@@ -5,13 +5,13 @@ class Minifier {
     public static function minify($html) {
         if (trim($html) === "") return $html;
 
-        // Rimuovi commenti HTML (tranne IE conditional comments)
+        // Remove HTML comments (except IE conditional comments)
         $html = preg_replace('/<!--(?!\s*(?:\[if[^\]]+\]|<!|>))(?:(?!-->).)*-->/s', '', $html);
 
-        // Rimuovi whitespace tra tag
+        // Remove whitespace between tags
         $html = preg_replace('~>\s+<~', '><', $html);
 
-        // Collassa spazi multipli in singoli spazi
+        // Collapse multiple spaces into single spaces
         $html = preg_replace('~\s{2,}~', ' ', $html);
 
         return trim($html);
