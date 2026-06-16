def287e25c286ffc89808e38a6e32f17b301a812
Federico Corrado Ghedina Krepo <fedeghe@gmail.com>
2026-06-17 00:20:09 +0200

    minifier fix

--- core/Minifier.php
+++ core/Minifier.php
@@ -5,6 +5,20 @@ class Minifier {
     public static function minify($html) {
         if (trim($html) === "") return $html;
 
+        $protected = array();
+        $counter = 0;
+
+        $protect = function($matches) use (&$protected, &$counter) {
+            $placeholder = "___PROTECTED_" . $counter . "___";
+            $protected[$placeholder] = $matches[0];
+            $counter++;
+            return $placeholder;
+        };
+
+        // Protect <script> and <style> contents from being mangled
+        $html = preg_replace_callback('~(<script\b[^>]*>)(.*?)(</script>)~is', $protect, $html);
+        $html = preg_replace_callback('~(<style\b[^>]*>)(.*?)(</style>)~is', $protect, $html);
+
         // Remove HTML comments (except IE conditional comments)
         $html = preg_replace('/<!--(?!\s*(?:\[if[^\]]+\]|<!|>))(?:(?!-->).)*-->/s', '', $html);
 
@@ -13,6 +27,11 @@ class Minifier {
 
         // Collapse multiple spaces into single spaces
         $html = preg_replace('~\s{2,}~', ' ', $html);
+
+        // Restore protected blocks
+        foreach ($protected as $placeholder => $content) {
+            $html = str_replace($placeholder, $content, $html);
+        }
 
         return trim($html);
     }
